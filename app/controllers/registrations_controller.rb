ef90da5bb4ee17e7fc946182c7aaa7de04bbd58c
Stefanie <stefcodes@gmail.com>
2020-01-16 11:13:42 -0800

    add strong params to registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,12 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+    private
+
+    def sign_up_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
 end
