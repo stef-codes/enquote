d0e484ecb1388c8affd796893c137f560dc78b93
Stefanie <stefcodes@gmail.com>
2020-01-16 17:41:49 -0800

    add book validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ApplicationRecord
     has_many :quotes
     has_many :users, through: :quotes
 
+    validates :author, presence: true
+    validates :name, uniqueness: true
+
     scope :has_author, -> { has_author.where("author IS NOT NULL") }
     
 end
