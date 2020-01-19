c90584b418a40954fa10e680dc192baeb0367885
Stefanie <stefcodes@gmail.com>
2020-01-18 18:28:38 -0800

    add styling

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
     has_many :users, through: :quotes
 
     validates :author, presence: true
-    validates :name, uniqueness: true
+    validates :name, :presence => true, :uniqueness => true
 
     accepts_nested_attributes_for :quotes, reject_if: :all_blank
     
