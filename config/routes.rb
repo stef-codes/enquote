50ed802db0b9366ff4fa84fc3e30b21afe4fde72
Stefanie <stefcodes@gmail.com>
2020-01-24 12:15:54 -0800

    remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,4 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
