8ea3bb9f8d8d975ab55a0993b235f2f463efeda2
Stefanie <stefcodes@gmail.com>
2020-01-24 12:13:25 -0800

    add changes for nested quotes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 
 
 
-
   root to: 'application#welcome'
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks' }
   devise_scope :user do 
